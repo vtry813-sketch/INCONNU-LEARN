8d2095383acec901a3c1f10539b7c2873ba0ea8a
tech927 <onnui688@gmail.com>
2025-10-24 16:19:32 -0300

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,36 @@
+export const APP_CONFIG = {
+  name: 'INCONNU LEARN',
+  description: 'Master JavaScript from Zero to Pro',
+  version: '1.0.0',
+  author: 'Inconnu Boy',
+  year: 2025
+};
+
+export const LEVEL_CONFIG = {
+  TOTAL_LEVELS: 50,
+  FREE_LEVELS: 10,
+  COINS_PER_LEVEL: 10,
+  REFERRAL_REWARD: 50,
+  WELCOME_COINS: 25
+};
+
+export const DIFFICULTY_COLORS = {
+  beginner: '#10b981',
+  intermediate: '#f59e0b',
+  advanced: '#ef4444',
+  expert: '#8b5cf6'
+};
+
+export const CONTACT_INFO = {
+  adminEmail: 'inconnuboytech@gmail.com',
+  whatsapp: '+17479445970',
+  telegram: '@just_me_inconnu',
+  youtube: 'https://www.youtube.com/@inconnuboy-b5h',
+  whatsappChannel: 'https://whatsapp.com/channel/0029VbBlpT396H4JPxNF7707'
+};
+
+export const SOCIAL_LINKS = {
+  github: 'https://github.com/INCONNU-BOY',
+  youtube: 'https://www.youtube.com/@inconnuboy-b5h',
+  whatsapp: 'https://wa.me/17479445970'
+};
