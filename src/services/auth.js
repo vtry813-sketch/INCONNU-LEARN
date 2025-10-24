b165391b4cb218418032abdafc4c94d6e5ba43a0
tech927 <onnui688@gmail.com>
2025-10-24 16:14:38 -0300

    Create auth.js

---
+++ src/services/auth.js
@@ -0,0 +1,31 @@
+import api from './api';
+
+export const authService = {
+  login: async (email, password) => {
+    const response = await api.post('/auth/login', { email, password });
+    return response.data;
+  },
+
+  signup: async (userData) => {
+    const response = await api.post('/auth/register', userData);
+    return response.data;
+  },
+
+  verifyToken: async (token) => {
+    const response = await api.post('/auth/verify', { token });
+    return response.data;
+  },
+
+  getMe: async () => {
+    const response = await api.get('/auth/me');
+    return response.data;
+  },
+
+  logout: () => {
+    localStorage.removeItem('token');
+  },
+
+  isAuthenticated: () => {
+    return !!localStorage.getItem('token');
+  },
+};
